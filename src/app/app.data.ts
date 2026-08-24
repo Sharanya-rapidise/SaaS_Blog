573fb44651f33be0055e299242d5c6b5583c521f
Sharanya-rapidise <sharanyademo@gmail.com>
2026-08-24 19:35:42 +0530

    Initial commit

---
+++ src/app/app.data.ts
@@ -0,0 +1,20 @@
+export interface list{
+    icon: string;
+    title: string;
+    icon2 ?: string;
+}
+
+export const List_Data: list[] = [
+    {icon:'home', title: 'Home'},
+    {icon:'search', title: 'Stakeholders', icon2:'keyboard_arrow_right'},
+    {icon:'account_balance', title: 'Client'},
+    {icon:'description', title: 'Resources'},
+    {icon:'trending_up', title: 'Opportunities'},
+    {icon:'task_alt', title: 'Optimize', icon2: 'keyboard_arrow_right'},
+    {icon: 'badge', title: 'Keywords'},
+    {icon: 'view_in_ar', title: 'Topics'},
+    {icon: 'trending_up', title: 'Performance'},
+    {icon: 'assignment_late', title: 'content'},
+    {icon: 'group', title: 'Team & Workflow', icon2: 'keyboard_arrow_right'},
+    {icon: 'settings', title: 'Settings'},
+]
---
+++ src/app/layout/sidebar/sidebar.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { list, List_Data } from '../../app.data';
+@Component({
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule],
+  standalone: true,
+  selector: 'app-sidebar',
+  styleUrl: './sidebar.css',
+  templateUrl: './sidebar.html',
+})
+export class Sidebar {
+  isOpened = true;
+
+  protected readonly lists: list[] = List_Data;
+}
